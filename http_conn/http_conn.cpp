f95bbdba555a0504b289f115de5decc05a345312
bianyishupian <z17866920355@163.com>
2023-05-11 22:37:21 +0800

    feat: new feature

--- http_conn/http_conn.cpp
+++ http_conn/http_conn.cpp
@@ -5,26 +5,6 @@
 int http_conn::m_epollfd = -1;         // 所有的socket上的事件都注册到一个epoll中
 int http_conn::m_user_count = 0;       // 统计用户的数量
 
-
-// 添加fd到epoll中，在http_conn中实现
-void add_fd(int epollfd,int fd,bool one_shot)
-{
-    epoll_event event;
-    event.events = EPOLLIN | EPOLLRDHUP;
-    event.data.fd = fd;
-    if(one_shot)
-    {
-        event.events |= EPOLLONESHOT;
-    }
-    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event);
-}
-// 从epoll中删除fd
-void remove_fd(int epollfd,int fd)
-{
-    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,0);
-    close(fd);
-}
-
 // 设置fd非阻塞
 void setNonblocking(int fd)
 {
@@ -32,6 +12,30 @@ void setNonblocking(int fd)
     int new_flag = old_flag | O_NONBLOCK;
     fcntl(fd,F_SETFL,new_flag);
 }
+
+// 添加fd到epoll中，在http_conn中实现
+void add_fd(int epollfd,int fd,bool one_shot)
+{
+    epoll_event event;
+    event.events = EPOLLIN | EPOLLRDHUP;
+    // event.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+    event.data.fd = fd;
+    if(one_shot)
+    {
+        event.events |= EPOLLONESHOT;
+    }
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event);
+    // 设置fd非阻塞
+    setNonblocking(fd);
+}
+
+// 从epoll中删除fd
+void remove_fd(int epollfd,int fd)
+{
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,0);
+    close(fd);
+}
+
 // 修改fd，重置EPOLLONESHOT事件
 void mod_fd(int epollfd,int fd,int ev)
 {
@@ -40,8 +44,6 @@ void mod_fd(int epollfd,int fd,int ev)
     event.data.fd = fd;
     event.events |= ev;
     epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&event);
-    // 设置文件描述符非阻塞
-    setNonblocking(fd);
 }
 
 
@@ -54,15 +56,6 @@ http_conn::~http_conn()
 
 }
 
-// 由线程池的工作线程调用，处理HTTP请求的入口函数
-void http_conn::process()
-{
-    // 解析HTTP请求
-
-    // need reWrite
-
-    // 生成相应
-}
 
 // 初始化连接
 void http_conn::init(int sockfd, const sockaddr_in & addr)
@@ -75,6 +68,11 @@ void http_conn::init(int sockfd, const sockaddr_in & addr)
     // 添加到epoll中
     add_fd(m_epollfd,m_sockfd,true);
     ++m_user_count; // 总用户加一
+
+    m_read_index = 0;
+
+    m_bytes_to_send = 0;
+    m_bytes_have_send = 0;
 }
 
 // 关闭连接
@@ -92,7 +90,32 @@ void http_conn::conn_close()
 bool http_conn::read()
 {
     printf("read\n");   // need reWriting
-
+    if(m_read_index >= READ_BUFFER_SIZE)
+    {
+        return false;
+    }
+    // 读取到的字节
+    int bytes_read = 0;
+    while (true)
+    {
+        bytes_read = recv(m_sockfd,m_read_buf + m_read_index,READ_BUFFER_SIZE - m_read_index,0);
+        if(bytes_read == -1)
+        {
+            if(errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                // 没有数据可读
+                break;
+            }
+            return false;
+        }
+        else if(bytes_read == 0)
+        {
+            // 对方关闭连接
+            return false;
+        }
+        m_read_index += bytes_read;
+    }
+    printf("read data:\n%s",m_read_buf);
     return true;
 }
 // 非阻塞写
@@ -103,5 +126,18 @@ bool http_conn::write()
     return true;
 }
 
+// 由线程池的工作线程调用，处理HTTP请求的入口函数
+void http_conn::process()
+{
+    // 解析HTTP请求
 
+    HTTP_CODE read_ret = process_read();
+    if(read_ret == NO_REQUEST)
+    {
+        mod_fd(m_epollfd, m_sockfd, EPOLLIN);
+        return;
+    }
+
+    // 生成相应
+}
 
--- http_conn/http_conn.h
+++ http_conn/http_conn.h
@@ -21,6 +21,51 @@
 class http_conn
 {
 public:
+    static int m_epollfd;       // 所有的socket上的事件都注册到一个epoll中
+    static int m_user_count;    // 统计用户的数量
+    static const int READ_BUFFER_SIZE = 2048;   // 读缓冲区大小
+    static const int WRITE_BUFFER_SIZE = 2048;  // 写缓冲区大小
+
+    // HTTP请求方法，这里只支持GET
+    enum METHOD {GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT};
+    
+     
+    /*
+        解析客户端请求时，主状态机的状态
+        CHECK_STATE_REQUESTLINE:当前正在分析请求行
+        CHECK_STATE_HEADER:当前正在分析头部字段
+        CHECK_STATE_CONTENT:当前正在解析请求体
+    */
+    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
+    
+    // 从状态机的三种可能状态，即行的读取状态，分别表示
+    // 1.读取到一个完整的行 2.行出错 3.行数据尚且不完整
+    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
+    
+    /*
+        服务器处理HTTP请求的可能结果，报文解析的结果
+        NO_REQUEST          :   请求不完整，需要继续读取客户数据
+        GET_REQUEST         :   表示获得了一个完成的客户请求
+        BAD_REQUEST         :   表示客户请求语法错误
+        NO_RESOURCE         :   表示服务器没有资源
+        FORBIDDEN_REQUEST   :   表示客户对资源没有足够的访问权限
+        FILE_REQUEST        :   文件请求,获取文件成功
+        INTERNAL_ERROR      :   表示服务器内部错误
+        CLOSED_CONNECTION   :   表示客户端已经关闭连接了
+    */
+    enum HTTP_CODE 
+    { 
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION 
+    };
+
+public:
     http_conn();
     ~http_conn();
 
@@ -29,12 +74,48 @@ public:
     void conn_close();  // 关闭连接
     bool read();        // 非阻塞读
     bool write();       // 非阻塞写
-public:
-    static int m_epollfd;       // 所有的socket上的事件都注册到一个epoll中
-    static int m_user_count;    // 统计用户的数量
+
+private:
+    HTTP_CODE process_read();    // 解析HTTP请求
+    bool process_write( HTTP_CODE ret );    // 填充HTTP应答
+
+    // 下面这一组函数被process_read调用以分析HTTP请求
+    HTTP_CODE parse_request_line( char* text );
+    HTTP_CODE parse_headers( char* text );
+    HTTP_CODE parse_content( char* text );
+    HTTP_CODE do_request();
+    char* get_line() { return m_read_buf + m_start_line; }
+    LINE_STATUS parse_line();
+
+    // 这一组函数被process_write调用以填充HTTP应答。
+    void unmap();
+    bool add_response( const char* format, ... );
+    bool add_content( const char* content );
+    bool add_content_type();
+    bool add_status_line( int status, const char* title );
+    bool add_headers( int content_length );
+    bool add_content_length( int content_length );
+    bool add_linger();
+    bool add_blank_line();
+
+    
+
 private:
     int m_sockfd;               // 该http连接的socket
     sockaddr_in m_address;      // 通信的socket地址
+    char m_read_buf[READ_BUFFER_SIZE];      // 读缓冲区
+    int m_read_index;                       // 标识读缓冲区已经读入的客户端数据的下一个位置
+    int m_checked_idx;                      // 当前正在分析的字符在读缓冲区中的位置
+    int m_start_line;                       // 当前正在解析的行的起始位置
+
+
+
+    char m_write_buf[WRITE_BUFFER_SIZE];    // 写缓冲区
+
+
+    int m_bytes_to_send;
+    int m_bytes_have_send;
+
 };
 
 
