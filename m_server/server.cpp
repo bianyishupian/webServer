a13bb7dba4985c7d72ed53f90ac151041d4c357e
bianyishupian <z17866920355@163.com>
2023-05-21 16:15:16 +0800

    修改：     m_server/server.cpp 	修改：     server

--- m_server/server.cpp
+++ m_server/server.cpp
@@ -73,11 +73,11 @@ void Server::log_write()
     {
         if(m_log_write == 1)    // 异步
         {
-            Log::get_instance()->init("./ServerLog", m_close_log, 2000, 800000, 800);
+            Log::get_instance()->init("log/ServerLog", m_close_log, 2000, 800000, 800);
         }
         else
         {
-            Log::get_instance()->init("./ServerLog", m_close_log, 2000, 800000, 0);
+            Log::get_instance()->init("log/ServerLog", m_close_log, 2000, 800000, 0);
         }
     }
 }
